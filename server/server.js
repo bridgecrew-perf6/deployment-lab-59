628a89e636b8702640f50d94e62754a3f7a82338
Wesley Lo <wesleylo1995@gmail.com>
2022-02-09 14:03:59 -0600

    added procfile

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('public'))
 
-const port = 4000
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
+const port = process.env.PORT || 4000
 app.listen(port, () => console.log(`server is running on port ${port}`))
