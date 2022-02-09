3b97d2a72696834a409479294a19a80c0dfd4164
Wesley Lo <wesleylo1995@gmail.com>
2022-02-09 14:08:22 -0600

    changed folder name

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.static('public'))
 
 app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 const port = process.env.PORT || 4000
